6f180e0100c93700ba40cfa4430659380fa24f53
teddysback <razvan.teslaru9@gmail.com>
2021-10-21 13:06:41 +0300

    small fix

--- scripts/grabReinstall.cmd
+++ scripts/grabReinstall.cmd
@@ -6,8 +6,8 @@ sc stop WdmDriver
 
 RunDLL32.Exe SETUPAPI.DLL,InstallHinfSection DefaultUninstall 132 %CD%\WdmDriver.inf
 
-xcopy /y "\\Pandora\Virtual Machines\SHARED\WdmDriver*" "c:\Users\test\Desktop\miniFlt\"
-xcopy /y "\\Pandora\Virtual Machines\SHARED\wdm_client*" "c:\Users\test\Desktop\miniFlt\"
+xcopy /y "\\Pandora\SHARED\WdmDriver*" "c:\Users\test\Desktop\miniFlt\"
+xcopy /y "\\Pandora\SHARED\wdm_client*" "c:\Users\test\Desktop\miniFlt\"
 
 RunDLL32.Exe syssetup,SetupInfObjectInstallAction DefaultInstall 128 %CD%\WdmDriver.inf
 
